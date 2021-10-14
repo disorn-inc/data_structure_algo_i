0a305fc0093893d4fe263cb682b39650eb22356a
disorn-inc <dirsorn1111@hotmail.com>
2021-10-14 18:45:35 +0700

    [add queue stack tree graph]

---
+++ data_structure/graph/graph.js
@@ -0,0 +1,30 @@
+class Graph {
+    constructor() {
+        this.numberOFNode = 0;
+        this.adjacentList = {};
+    }
+    
+    addVertex(node) {
+        this.adjacentList[node] = [];
+        this.numberOFNode++;
+    }
+
+    addEdge(node1, node2) {
+        this.adjacentList[node1].push(node2);
+        this.adjacentList[node2].push(node1);
+    }
+
+    showConnections() {
+        const allNode = Object.keys(this.adjacentList);
+
+        for(let node of allNode) {
+            let nodeConnections = this.adjacentList[node];
+            let connections = "";
+            let vertex;
+            for(vertex of nodeConnections) {
+                connections += vertex + " ";
+            }
+            console.log(node + "-->" + connections);
+        }
+    }
+}
